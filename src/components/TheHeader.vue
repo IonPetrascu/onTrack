9400008c597f05be5b6d1c40283907b98c6ff7e5
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-24 12:04:41 +0300

    feat: divided everything into components

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheLogo from './TheLogo.vue'
+import TheHeaderProgress from './TheHeaderPorgress.vue'
+</script>
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white">
+    <TheLogo />
+    <TheHeaderProgress />
+  </header>
+</template>
