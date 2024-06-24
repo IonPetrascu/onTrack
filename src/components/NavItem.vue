e6a90d72dbc0277c3629e9d7c71f44580463820a
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-24 12:24:23 +0300

    feat: add NavItem and use $attrs

---
+++ src/components/NavItem.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
+<template>
+  <li>
+    <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+      <slot></slot>
+      ></a
+    >
+  </li>
+</template>
+<style scoped></style>
