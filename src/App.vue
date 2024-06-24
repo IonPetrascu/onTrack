dc7b8fe02bbff4d25dc9ca996ce5e1a85865ae4e
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-24 16:52:14 +0300

    feat: add timeline options

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import { PAGE_ACTIVITIES, PAGE_PROGRESS, PAGE_TIMELINE } from './constants'
+import { generateTimelineItem, normalizePageHash } from './functions'
 import { ref } from 'vue'
 import TheNav from './components/TheNav.vue'
 import TheHeader from './components/TheHeader.vue'
 import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
-import { normalizePageHash } from './functions'
 
 const currentPage = ref(normalizePageHash())
+const timelineItems = generateTimelineItem()
 
 function goTo(page) {
   currentPage.value = page
@@ -18,9 +19,15 @@ function goTo(page) {
 <template>
   <TheHeader @go-to-progress="goTo(PAGE_PROGRESS)" @go-to-timeline="goTo(PAGE_TIMELINE)" />
   <main class="flex-grow">
-    <div v-show="currentPage === PAGE_TIMELINE"><TheTimeline /></div>
-    <div v-show="currentPage === PAGE_ACTIVITIES"><TheActivities /></div>
-    <div v-show="currentPage === PAGE_PROGRESS"><TheProgress /></div>
+    <div v-show="currentPage === PAGE_TIMELINE">
+      <TheTimeline :timeline-items="timelineItems" />
+    </div>
+    <div v-show="currentPage === PAGE_ACTIVITIES">
+      <TheActivities />
+    </div>
+    <div v-show="currentPage === PAGE_PROGRESS">
+      <TheProgress />
+    </div>
   </main>
   <TheNav @navigate="(page) => goTo(page)" :current-page="currentPage" />
 </template>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,22 +1,16 @@
 <script setup>
-import { generateTimelineItem } from '@/functions'
-const timelineItems = generateTimelineItem()
+import TimelineItem from '@/components/TimelineItem.vue'
+defineProps(['timelineItems'])
 </script>
 <template>
   <div class="mt-7">
     <ul>
-      <li
-        class="relative flex flex-col gap-2 border-t border-gray-200 px-4 py-10"
-        :key="hour"
-        v-for="{ hour } in timelineItems"
+      <TimelineItem
+        :key="timelineItem.hour"
+        :timeline-item="timelineItem"
+        v-for="timelineItem in timelineItems"
       >
-        <a
-          class="text absolute -top-4 left-1/2 -translate-x-1/2 rounded bg-gray-100 px-2 font-mono text-gray-500"
-          href="#"
-        >
-          {{ hour }}:00</a
-        >
-      </li>
+      </TimelineItem>
     </ul>
   </div>
 </template>
