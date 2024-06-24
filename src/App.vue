b7872b37bcdff9fa957ab3fb78a7a227d2d91338
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-24 11:17:33 +0300

    first commit and added header

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white">
+    <a href="#">
+      <img class="h-9" src="./assets/img/logo.png" alt="Logo " />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="true" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
+</template>
+
+<style scoped></style>
